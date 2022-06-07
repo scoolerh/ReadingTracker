784a1515e41a1ead3bf49ff32b8182ec72e9c7ce
Han <hannahscooler@gmail.com>
2022-06-06 19:06:32 -0500

    adding webapp.py and more static webpages

---
+++ webapp.py
@@ -0,0 +1,46 @@
+import flask 
+from flask import render_template, request 
+import json
+import sys
+
+app = flask.Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def home():
+    '''
+    Run the home page of the website. 
+    '''
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    '''
+    Run the about page of the website. 
+    '''
+    return render_template('about.html')
+
+@app.route('/pagetracker')
+def pages():
+    ''' 
+    Run the page tracker. 
+    '''
+    return render_template('pages.html')
+
+@app.route('/completedbooks')
+def books():
+    '''
+    Run the bookshelf. 
+    '''
+    return render_template('books.html')
+
+@app.route('/stats')
+def stats():
+    '''
+    Run the statistics. 
+    '''
+    return render_template('stats.html')
+
+if __name__ == '__main__': 
+    app.run()
